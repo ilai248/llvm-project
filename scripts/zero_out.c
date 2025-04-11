355145fa93ccd0069b011bff6fc7ad8b05e4aef3
ilai248 <ilaish27@gmail.com>
2025-04-11 20:42:44 +0300

    Made hello world work

---
+++ scripts/zero_out.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void arg6(int a, int b, int c, int d, int e, int f) { }
+
+void (*arg6_)(int, int, int, int, int, int) = arg6;
+
+int main() {
+    arg6_(1, 2, 3, 4, 5, 6);
+    printf("%08x %08x %08x %08x %08x\n");
+}
+
