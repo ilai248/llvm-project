73d828859f2c617a68367f849ea390f271f240b3
ilai248 <ilaish27@gmail.com>
2025-04-12 15:41:38 +0300

    commit before changing va_list

---
+++ scripts/test-va-copy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void test_fn(const char* s, ...) {
+    va_list vl, vl2;
+    va_start(vl, s);
+    va_copy(vl2, vl);
+    vfprintf(stdout, s, vl);
+    vfprintf(stdout, s, vl2);
+    va_end(vl);
+}
+
+int main() {
+    test_fn("Foo %d %d %d %d %d %d %d %d\n", 1, 2, 3, 4, 5, 6, 7, 8);
+    return 0;
+}
